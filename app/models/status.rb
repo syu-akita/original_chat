0886b385f98826119d3c49e0980959860ee1fb64
syu-akita <syu.akita.0612@gmail.com>
2021-01-19 12:01:10 +0900

    register-wrap cssの追加

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,7 +2,7 @@ class Status < ActiveHash::Base
   self.data = [
     { id: 1, number: '---' },
     { id: 2, number: '先生' },
-    { id: 3, number: '生徒' }
+    { id: 3, number: '保護者' }
   ]
   include ActiveHash::Associations
   has_many :users
