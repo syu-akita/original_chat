90e1bace0f9c56c8ed656433f398370acc110832
syu-akita <syu.akita.0612@gmail.com>
2021-01-20 18:49:54 +0900

    userモデルのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence true do
+    validates :name
+    validates :status_id
+    validates :group_id
+    validates :password_confirmation
+  end
   extend ActiveHash::Associations::ActiveRecordExtensions
-  
   belongs_to :group
   belongs_to :status
 end
