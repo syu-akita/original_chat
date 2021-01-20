bf3e217bdb8b9d2db148322fc9c94142861ae3f2
syu-akita <syu.akita.0612@gmail.com>
2021-01-20 17:45:07 +0900

    ログインページのエラー文の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   extend ActiveHash::Associations::ActiveRecordExtensions
+  
   belongs_to :group
   belongs_to :status
 end
