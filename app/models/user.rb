702678b62363ca6033ca9e23e9e09bce914d9724
syu-akita <syu.akita.0612@gmail.com>
2021-01-21 14:32:53 +0900

    roomテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  with_options presence true do
+  with_options presence: true do
     validates :name
     validates :status_id
     validates :group_id
